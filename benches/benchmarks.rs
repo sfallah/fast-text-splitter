cb7b181537c67eeb10103a88316fcf46b88065e1
Saba Fallah <sfallah@users.noreply.github.com>
2024-06-24 14:12:10 +0200

    bigger content for benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -8,7 +8,8 @@ use fast_text_splitter::ws_tokenizer::WSTokenizer;
 use std::fs;
 
 pub fn hf_merged_tree_lite_res_benchmark(c: &mut Criterion) {
-    let data_path = "tests/test_data/superlinear.txt";
+    //let data_path = "tests/test_data/superlinear.txt";
+    let data_path = "tests/test_data/United_States.txt";
 
     let binding = fs::read(data_path).unwrap();
     let data = binding.as_slice();
@@ -30,7 +31,9 @@ pub fn hf_merged_tree_lite_res_benchmark(c: &mut Criterion) {
 }
 
 pub fn ws_tree_split_lite_benchmark(c: &mut Criterion) {
-    let data_path = "tests/test_data/superlinear.txt";
+    //let data_path = "tests/test_data/superlinear.txt";
+    let data_path = "tests/test_data/United_States.txt";
+
 
     let binding = fs::read(data_path).unwrap();
     let data = binding.as_slice();
@@ -72,7 +75,9 @@ pub fn ws_tree_split_lite_benchmark(c: &mut Criterion) {
 }
 
 pub fn none_tree_split_lite_benchmark(c: &mut Criterion) {
-    let data_path = "tests/test_data/superlinear.txt";
+    //let data_path = "tests/test_data/superlinear.txt";
+    let data_path = "tests/test_data/United_States.txt";
+
 
     let binding = fs::read(data_path).unwrap();
     let data = binding.as_slice();
